ba59dadd2a99633fc3e99fd502b449822ea5a7e9
Titoun3 <73279480+Titoun3@users.noreply.github.com>
2021-04-12 15:22:14 +0200

    V2

--- Pendu.py
+++ Pendu.py
@@ -1,6 +1,6 @@
 """
-Project : Pendu
-Version : 1.0
+Projet : Pendu
+Version : 2.0
 Author : Titouan Schotté
 Python 3.9
 """
@@ -9,20 +9,49 @@ Python 3.9
 import random
 import os
 import color
+from os import system, name
+
+print("==========================================\n>> #--" + color.CGREEN + "Bienvenue dans le jeu du pendu" + color.CEND + "--# <<\n==========================================")
 
 
 
-play = True
-while play:
-        os.system('cls')
-        print("==========================================")
-        print(">> #--" + color.CGREEN  +"Bienvenue dans le jeu du pendu"+ color.CEND +  "--# <<")
-        print("==========================================")
+#Nouvelle partie ?
+def new_game():
 
-        # choix d'un mot au hasard dans un dictionnaire : ici liste_francais.txt
-        randomnumber = int(random.randint(0 , 27739))
-        book = open("liste_francais.txt", "r")
-        solution = book.readlines()[randomnumber]
+    new_game = "y"
+    new_game = input("Nouvelle partie ??? (y/n) \n -> ")
+    if new_game == "y":
+        return game()
+    if new_game != "y":
+        print("Merci d'avoir joué à mon jeu !!")
+        print(
+            color.CRED + "Si un quelconque beug est détecté contactez " + color.CEND + color.CYELLOW + "Titoune#1870" + color.CEND + color.CRED + " sur Discord" + color.CEND)
+        print(color.CGREEN + "Si vous avez des suggestions n'hésitez pas aussi c'est une V1 !!" + color.CEND)
+        exit()
+
+
+
+
+
+
+
+
+
+#JEU PRINCIPAL
+def game():
+
+
+
+        # choix d'un mot au hasard dans un dictionnaire
+        randomnumber = random.randint(0 , 50)
+        liste = ['protocole', 'injection', 'cryptographie', 'web', 'internet', 'http', 'cryptomonnaie', 'bitcoin',
+                 'hacking', 'php', 'html', 'css', 'sql', 'java', 'javascript',
+                 'ddos', 'bug-bounty', 'bug', 'c', 'c sharp', 'c++', 'ruby', 'python', 'dart', 'ethereum', 'script',
+                 'snowden', 'constructeur', 'classe', 'variable', 'fonction',
+                 'net', 'fishing', 'informatique', 'enigma', 'framework', 'heritage', 'liste', 'github', 'sniffer',
+                 'tcp', 'udp', 'binaire', 'hexadecimal', 'donnee', 'metadonnee',
+                 'paquet', 'serveur', 'client', 'database']
+        solution = liste[randomnumber]
         #on définit le nombre de tentatives que l'utilisateur va avoir pour réussir :
         tentatives = 7
         affichage = ""
@@ -35,7 +64,6 @@ while play:
 
         for l in solution:
           affichage = affichage + "_ "
-
         print("\n"+ color.CITALIC + "=> Vous avez {} tentatives pour réussir ! GOOD LUCK ;)".format(tentatives_string) + color.CEND)
         while tentatives > 0:
           print("\n"+ color.CUNDERLINE + "Mot à deviner :" + color.CEND + "", affichage)
@@ -43,13 +71,13 @@ while play:
 
           if proposition in solution:
               lettres_trouvees = lettres_trouvees + proposition
-              os.system('cls')
+
               print('-> Bravo vous avez trouvé la lettre "{}" .'.format(proposition))
 
         #différentes tentatives, -1 tentative à chaque essai !
           else:
             tentatives = tentatives - 1
-            os.system('cls')
+
             print("-> Nope\n")
 
             if tentatives==0:
@@ -66,6 +94,13 @@ while play:
                 print(color.CYELLOW+"/"+color.CEND+color.CGREY+"||"+color.CEND+"       "+color.CBEIGE+"/ \  "+color.CEND+"   ")
             if tentatives<=6:
                 print(color.CGREY+"==============\n"+color.CEND)
+            if tentatives == 0:
+                print('Le mot était : ' + color.CBLUE + color.CBOLD + '{}'.format(solution) + color.CEND + color.CEND)
+
+                print(color.CVIOLET + "=====================" + color.CEND)
+                print("    * " + color.CBACKRED + "GAME OVER" + color.CEND + " *    ")
+                print(color.CVIOLET + "=====================" + color.CEND)
+                new_game()
 
 #Lettre trouvée
 
@@ -77,23 +112,15 @@ while play:
                   affichage += "_ "
 #Win
           if "_" not in affichage:
-            os.system('cls')
+
             print(">>> Gagné! <<<")
             print("Le mot était bien {}".format(solution))
-            break
-
-
+            new_game()
 #Lose
-        print('Le mot était : '+color.CBLUE + color.CBOLD + '{}'.format(solution) + color.CEND + color.CEND)
-        os.system('timeout 10')
-        print(color.CVIOLET+"====================="+color.CEND)
-        print("    * "+color.CBACKRED+"GAME OVER"+color.CEND+" *    ")
-        print(color.CVIOLET+"====================="+color.CEND)
-        new_game = input("Nouvelle partie ??? (y/n) \n -> ")
 
 
-        if new_game != "y" :
-            print("Merci d'avoir joué à mon jeu !!")
-            print(color.CRED+"Si un quelconque beug est détecté contactez "+color.CEND+color.CYELLOW+"Titoune#1870"+color.CEND+color.CRED+" sur Discord"+color.CEND)
-            print(color.CGREEN+"Si vous avez des suggestions n'hésitez pas aussi c'est une V1 !!"+color.CEND)
-            exit()
+
+
+
+
+game()
