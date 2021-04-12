a35665f3146e59a446f20c2828873baaa7f932ed
Titoun3 <73279480+Titoun3@users.noreply.github.com>
2021-04-12 22:30:26 +0200

    V2.4

--- Pendu.py
+++ Pendu.py
@@ -9,7 +9,7 @@ Python 3.9
 import random
 import os
 import color
-from os import system, name
+from dico import liste
 
 print("==========================================\n>> #--" + color.CGREEN + "Bienvenue dans le jeu du pendu" + color.CEND + "--# <<\n==========================================")
 
@@ -44,26 +44,9 @@ def game():
 
 
         # choix d'un mot au hasard dans un dictionnaire
-        randomnumber = random.randint(0 , 142)
-        liste = ['protocole', 'injection', 'cryptographie', 'web', 'internet', 'http', 'cryptomonnaie', 'bitcoin',
-                 'hacking', 'php', 'html', 'css', 'sql', 'java', 'javascript',
-                 'ddos', 'bug-bounty', 'bug', 'c', 'c sharp', 'c++', 'ruby', 'python', 'dart', 'ethereum', 'script',
-                 'snowden', 'constructeur', 'classe', 'variable', 'fonction',
-                 'net', 'fishing', 'informatique', 'enigma', 'framework', 'heritage', 'liste', 'github', 'sniffer',
-                 'tcp', 'udp', 'binaire', 'hexadecimal', 'donnee', 'metadonnee',
-                 'paquet', 'serveur', 'client', 'database', 'ip', 'adresse mac', 'arp', 'pare feu', 'linux', 'kali linux',
-                 'arch linux', 'thor','CPU', 'GPU', 'carte mere', ' carte graphique', 'processeur', 'technologie', 'mysql', 'ionic', 'flutter',
-                 'data-mining', 'litecoin', 'ram', 'jet brains', 'data', 'big data', 'hertz', 'intelligence artificielle',
-                 'internet protocol', 'bit', 'octet', 'megaoctet', 'gigaoctet','kilooctet', 'teraoctet', 'application',
-                 'reseaux-sociaux', 'terminal', 'cmd', 'log', 'run', 'utf-8', "methode d'encodage", 'event', 'Override', 'requete', 'data scientist',
-                 'cybersecurite', 'developpeur', 'localhost', 'navigateur', 'moteur de recherche', 'main', 'index', 'anti-virus', 'malware', 'bios',
-                 'ransomware', 'spyware', 'file', 'software', 'logiciel', 'pixel', 'trojan', 'cheval de troie', 'keylogger', 'virus', 'algorithme',
-                 'cookie', 'base de donnees','peer to peer', 'plugin', 'code', 'proxy', 'vpn', 'cracking', 'url', 'http', 'https', 'lien hypertext',
-                 'statistiques', 'console', 'csv', 'xml','javascript object notation', 'macos', 'debug',
-                 'password', 'pirate', 'login', 'mot de passe', 'wireless fidelity', 'wifi', 'ethernet', 'lan']
 
-
-        solution = liste[randomnumber]
+        solution = liste()
+        print(solution)
         #on définit le nombre de tentatives que l'utilisateur va avoir pour réussir :
         tentatives = 7
         affichage = ""
