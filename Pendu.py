173f21db7372cb71ab056fe226ab56e752dc7a0d
Titoun3 <73279480+Titoun3@users.noreply.github.com>
2021-04-12 22:18:35 +0200

    V2.3

--- Pendu.py
+++ Pendu.py
@@ -44,14 +44,25 @@ def game():
 
 
         # choix d'un mot au hasard dans un dictionnaire
-        randomnumber = random.randint(0 , 58)
+        randomnumber = random.randint(0 , 142)
         liste = ['protocole', 'injection', 'cryptographie', 'web', 'internet', 'http', 'cryptomonnaie', 'bitcoin',
                  'hacking', 'php', 'html', 'css', 'sql', 'java', 'javascript',
                  'ddos', 'bug-bounty', 'bug', 'c', 'c sharp', 'c++', 'ruby', 'python', 'dart', 'ethereum', 'script',
                  'snowden', 'constructeur', 'classe', 'variable', 'fonction',
                  'net', 'fishing', 'informatique', 'enigma', 'framework', 'heritage', 'liste', 'github', 'sniffer',
                  'tcp', 'udp', 'binaire', 'hexadecimal', 'donnee', 'metadonnee',
-                 'paquet', 'serveur', 'client', 'database', 'ip', 'adresse mac', 'arp', 'pare feu', 'linux', 'kali linux', 'arch linux', 'thor'] #58 words
+                 'paquet', 'serveur', 'client', 'database', 'ip', 'adresse mac', 'arp', 'pare feu', 'linux', 'kali linux',
+                 'arch linux', 'thor','CPU', 'GPU', 'carte mere', ' carte graphique', 'processeur', 'technologie', 'mysql', 'ionic', 'flutter',
+                 'data-mining', 'litecoin', 'ram', 'jet brains', 'data', 'big data', 'hertz', 'intelligence artificielle',
+                 'internet protocol', 'bit', 'octet', 'megaoctet', 'gigaoctet','kilooctet', 'teraoctet', 'application',
+                 'reseaux-sociaux', 'terminal', 'cmd', 'log', 'run', 'utf-8', "methode d'encodage", 'event', 'Override', 'requete', 'data scientist',
+                 'cybersecurite', 'developpeur', 'localhost', 'navigateur', 'moteur de recherche', 'main', 'index', 'anti-virus', 'malware', 'bios',
+                 'ransomware', 'spyware', 'file', 'software', 'logiciel', 'pixel', 'trojan', 'cheval de troie', 'keylogger', 'virus', 'algorithme',
+                 'cookie', 'base de donnees','peer to peer', 'plugin', 'code', 'proxy', 'vpn', 'cracking', 'url', 'http', 'https', 'lien hypertext',
+                 'statistiques', 'console', 'csv', 'xml','javascript object notation', 'macos', 'debug',
+                 'password', 'pirate', 'login', 'mot de passe', 'wireless fidelity', 'wifi', 'ethernet', 'lan']
+
+
         solution = liste[randomnumber]
         #on définit le nombre de tentatives que l'utilisateur va avoir pour réussir :
         tentatives = 7
